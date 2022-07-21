b2a1b4cdb565155d2848e9d43874afa909516f47
Marcos <marcoswestwood@gmail.com>
2022-07-20 22:01:42 -0300

    UC11 Concluida

---
+++ Chapter/Chapter/Contexts/ChapterContext.cs
@@ -0,0 +1,27 @@
+﻿using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Contexts
+{
+    public class ChapterContext : DbContext
+    {
+    public ChapterContext()
+        {
+        }
+        public ChapterContext(DbContextOptions<ChapterContext> options) : base(options)
+        {
+        }
+        //vamos utilizar este metodo para configurar o banco de dados
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        if (!optionsBuilder.IsConfigured)
+            {
+                // cada provedor tem sua sintaxe para especificacao
+                optionsBuilder.UseSqlServer("Data Source = DELLG15\\SQLEXPRESS; initial catalog = Chapter; Integrated Security = true");
+            }
+        }
+        //DbSet representa as entidades que serao utilizadas nas operacoes de leitura, criacao, atualizacao e delecao. 
+        public DbSet<Livro> Livros { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ Chapter/Chapter/Controllers/UsuarioController.cs
@@ -0,0 +1,100 @@
+﻿using Chapter.Interfaces;
+using Chapter.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapter.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioRepository _iUsuarioRepository;
+
+        public UsuarioController (IUsuarioRepository usuarioRepository)
+        {
+            _iUsuarioRepository = usuarioRepository;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_iUsuarioRepository.Listar());
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Usuario usuarioEncontrado = _iUsuarioRepository.BuscarPorId(id);
+                if(usuarioEncontrado == null)
+                        return NotFound();
+                return Ok(usuarioEncontrado);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario usuario)
+        {
+            try
+            {
+                _iUsuarioRepository.Cadastrar(usuario);
+                return StatusCode(201);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Alterar(int id, Usuario usuario)
+        {
+            try
+            {
+                _iUsuarioRepository.Atualizar(id, usuario);
+                return Ok("Usuario Alterado");
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _iUsuarioRepository.Deletar(id);
+                return Ok("Usuario Deletado");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
